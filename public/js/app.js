0bce2e9dc8ef5dfb7ea557a8f283a9a56838befe
Duc <ramenatmidnight9@gmail.com>
2014-10-07 12:27:52 +0700

    added bower, front page image auto resize now

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
 // glue all components - services, routes, resources, controllers - together for the Angular app.
 // use dependency injection and setup the Angular application
 // mbwApp is in index
-angular.module('mbwApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'BestCtrl']);
+angular.module('mbwApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'BestCtrl', 'angular-wurfl-image-tailor']);
